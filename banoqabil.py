d897768f360b5ec370fd1332a71226ad148c61c3
Alishbakhan313201 <Alishbakhan817a@gmail.com>
2024-11-03 17:13:56 +0500

    Add files via upload
    i make this file in python V .S code

---
+++ banoqabil.py
@@ -0,0 +1,42 @@
+a# QUESTION 1
+number1=10
+number2=14
+number3=16
+
+sum=number1+number2+number3  #sum
+print("sum:", sum)
+
+diffrence=number1-number2-number3  #diffrence
+print("difference:", diffrence) 
+
+product=number1*number2*number3  #product
+print("product:", product)
+
+average=number1+number2+number3  #average
+print("average:", average)
+
+
+
+# QUESTION 2
+#triangle area
+base=8
+height=16
+area=0.5*base*height
+print("area of triangle",area)
+#square area
+side=5
+area=side*side
+print("area of square",area)
+#circle area
+pi=3.14
+radius=10
+area=pi*radius*radius
+print("area of circle",area)
+#rectangle area
+length=6
+width=3
+area=length*width
+print("area of rectangle",area)
+
+
+
